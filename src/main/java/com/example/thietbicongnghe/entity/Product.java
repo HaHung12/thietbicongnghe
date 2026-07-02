1e6958247efcf9acee975329f7e97b7d5fb3fed0
HaHung12 <huuhung20112005@gmail.com>
2026-07-02 08:27:21 +0700

    test

--- src/main/java/com/example/thietbicongnghe/entity/Product.java
+++ src/main/java/com/example/thietbicongnghe/entity/Product.java
@@ -10,7 +10,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     private String name;
     private String brand;
@@ -29,11 +29,11 @@ public class Product {
         this.createdAt = LocalDateTime.now();
     }
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
